a7202bc2ee396763d3412ce39f61ee8d737c752b
ZaneC04 <zenithzsc@gmail.com>
2025-11-25 19:20:40 -0500

    fixed linting errors caused by adding uml and loom links

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,46 +1,46 @@
 class Pokemon {
-    #health = 100;
+  #health = 100;
 
-    #level = 1;
+  #level = 1;
 
-    static allPokemon = [];
+  static allPokemon = [];
 
-    constructor(name, type) {
-        this.name = name;
-        this.type = type;
-        Pokemon.allPokemon.push(this);
-    }
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+    Pokemon.allPokemon.push(this);
+  }
 
-    get health() {
-        return this.#health;
-    }
+  get health() {
+    return this.#health;
+  }
 
-    get level() {
-        return this.#level;
-    }
+  get level() {
+    return this.#level;
+  }
 
-    levelUp() {
-        this.#level += 1;
-        this.#health += 10;
-        console.log(`${this.name} leveled up to level ${this.#level}!`);
-    }
+  levelUp() {
+    this.#level += 1;
+    this.#health += 10;
+    console.log(`${this.name} leveled up to level ${this.#level}!`);
+  }
 
-    isFainted() {
-        return this.#health < 1;
-    }
+  isFainted() {
+    return this.#health < 1;
+  }
 
-    attack(targetPokemon) {
-        targetPokemon.#health -= 10 * this.#level;
-        console.log(`${this.name} attacked ${targetPokemon.name}!`);
-    }
+  attack(targetPokemon) {
+    targetPokemon.#health -= 10 * this.#level;
+    console.log(`${this.name} attacked ${targetPokemon.name}!`);
+  }
 
-    static getTotalPokemon() {
-        return Pokemon.allPokemon.length;
-    }
+  static getTotalPokemon() {
+    return Pokemon.allPokemon.length;
+  }
 
-    static findByName(name) {
-        return Pokemon.allPokemon.find((pokemon) => pokemon.name === name);
-    }
+  static findByName(name) {
+    return Pokemon.allPokemon.find((pokemon) => pokemon.name === name);
+  }
 }
 
 // TEST YOUR CODE HERE
